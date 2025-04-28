c14d286bd64331738f2b2932cb4ae587b419cce4
Aymeric LEGER ACHARD <aymeric.leger-achard@3310street.com>
2025-04-28 17:28:04 +0200

    Fix api mail

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,8 +1,8 @@
-import {NextResponse} from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 import mailjet from 'node-mailjet';
 import * as process from "process";
 
-export async function POST(req: NextResponse) {
+export async function POST(req: NextRequest) {
 	const body = await req.json();
 	const name = body['name'];
 	const email = body['email'];
